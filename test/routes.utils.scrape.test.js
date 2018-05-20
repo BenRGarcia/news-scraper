a7ebcb7405d1d5da3743c7bda78c51853eeecc8c
Ben Garcia <ben@seebenprogram.com>
2018-05-20 16:15:52 -0400

    build test scaffolding

---
+++ test/routes.utils.scrape.test.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-unused-expressions */
+var chai = require('chai')
+var expect = chai.expect
+var chaiHttp = require('chai-http')
+chai.use(chaiHttp)
+
+describe('`scrape.js`', function () {
+  it('should return a promise', function () {
+
+  })
+
+  it('should `resolve` promise with scraped html from given url', function () {
+
+  })
+
+  it('should `reject` promise with error if error', function () {
+
+  })
+})
