2d00f1ccc1937c9136cc00ede25956edeaaf1900
Ben Garcia <ben@seebenprogram.com>
2018-05-20 15:17:04 -0400

    initial commit

---
+++ test/routes.utils.normalize.test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect
+var normalize = require('../routes/utils/normalize.js')
+
+describe('`normalize.js`', function () {
+  it('should normalize scraped HTML data', function () {
+
+  })
+})
