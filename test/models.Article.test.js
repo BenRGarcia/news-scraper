2e4a1085d4fa392c2e69d55e3f66a8a1a89e6fe5
Ben Garcia <ben@seebenprogram.com>
2018-05-20 14:21:20 -0400

    Build tests

--- test/models.Article.test.js
+++ test/models.Article.test.js
@@ -1,2 +1,29 @@
+/* eslint-disable no-unused-expressions */
 var expect = require('chai').expect
 var Article = require('../models').Article
+
+describe('Model: Article', function () {
+  it('should be invalid if `title` is empty', function (done) {
+    var a = new Article()
+    a.validate(function (err) {
+      expect(err.errors.title).to.exist
+      done()
+    })
+  })
+
+  it('should be invalid if `link` is empty', function (done) {
+    var a = new Article()
+    a.validate(function (err) {
+      expect(err.errors.link).to.exist
+      done()
+    })
+  })
+
+  it('should be valid if `comments` is empty', function (done) {
+    var a = new Article()
+    a.validate(function (err) {
+      expect(err.errors.comments).to.not.exist
+      done()
+    })
+  })
+})
--- test/models.Comment.test.js
+++ test/models.Comment.test.js
@@ -1,2 +1,13 @@
+/* eslint-disable no-unused-expressions */
 var expect = require('chai').expect
 var Comment = require('../models').Comment
+
+describe('Model: Comment', function () {
+  it('should be invalid if `text` is empty', function (done) {
+    var c = new Comment()
+    c.validate(function (err) {
+      expect(err.errors.text).to.exist
+      done()
+    })
+  })
+})
