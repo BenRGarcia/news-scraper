857bc41e880ad4419bc684ad83d909bb57fc5145
Ben Garcia <ben@seebenprogram.com>
2018-05-20 21:26:46 -0400

    replace await with return

--- routes/utils/query.js
+++ routes/utils/query.js
@@ -1,32 +1,32 @@
 var db = require('../../models')
 
 async function getSavedArticles () {
-  await db.Article.find({})
+  return db.Article.find({})
     .populate('comments')
     .then(articles => articles)
     .catch(err => err)
 }
 
 async function saveArticle ({ title, preview, link }) {
-  await db.Article.create({ title, preview, link })
+  return db.Article.create({ title, preview, link })
     .then(newArticle => newArticle)
     .catch(err => err)
 }
 
 async function deleteArticle ({ _id }) {
-  await db.Article.findOneAndDelete({ _id })
+  return db.Article.findOneAndDelete({ _id })
     .then(deletedArticle => db.Comments.deleteMany({ _id: { $in: deletedArticle.comments } }))
     .catch(err => err)
 }
 
 async function addComment ({ text, _id }) {
-  await db.Comment.create({ text })
+  return db.Comment.create({ text })
     .then(newComment => db.Article.findOneAndUpdate({ _id }, { $push: { comments: newComment._id } }))
     .catch(err => err)
 }
 
 async function deleteComment ({ _idComment, _idArticle }) {
-  await db.Comment.findOneAndRemove({ _idComment })
+  return db.Comment.findOneAndRemove({ _idComment })
     .then(() => db.Article.update({ _idArticle }, { $pull: { comments: _idComment } }))
     .catch(err => err)
 }
