d5f9ad936c0d823051a9800fe39f88ff275c47ab
Ben Garcia <ben@seebenprogram.com>
2018-05-20 21:15:03 -0400

    refactor for brevity

--- routes/utils/scrape.js
+++ routes/utils/scrape.js
@@ -1,11 +1,3 @@
 const axios = require('axios')
 
-module.exports = url => {
-  try {
-    return new Promise((resolve, reject) => {
-      axios(/* ... */)
-    })
-  } catch (err) {
-    console.error(err)
-  }
-}
+module.exports = async url => axios.get(url)
