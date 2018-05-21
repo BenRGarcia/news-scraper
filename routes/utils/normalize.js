dfc4aa1be6ed7caac026b1d899540b1de8a34b75
Ben Garcia <ben@seebenprogram.com>
2018-05-20 21:14:52 -0400

    Build normalize() method

--- routes/utils/normalize.js
+++ routes/utils/normalize.js
@@ -1,5 +1,14 @@
 var cheerio = require('cheerio')
 
-module.exports = (html) => {
+module.exports = html => {
+  var results = []
   var $ = cheerio.load(html)
+  $('div.latestnewsitem').each((i, el) => {
+    var article = $(el).children().children('.col-lg-8')
+    var title = $(article).children('h4').children('a').attr('href').trim()
+    var link = $(article).children('h4').children('a').text()
+    var preview = $(article).children('p').text().trim()
+    results.push({ title, link, preview })
+  })
+  return results
 }
