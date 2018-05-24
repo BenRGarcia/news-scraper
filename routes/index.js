d0caa07cabd4fd953de0f5f8ca1a510c0b196133
Ben Garcia <ben@seebenprogram.com>
2018-05-24 14:50:13 -0400

    build html route for saved articles

--- routes/index.js
+++ routes/index.js
@@ -12,8 +12,10 @@ router.get('/', (req, res, next) => res.render('index'))
 // 'SAVED ARTICLES' page
 router.get('/articles/saved', (req, res, next) => {
   // Retrieve articles from 'savedArticles' collection (if any)...
-  // ...then render results to page
-  res.render('saved', {})
+  getSavedArticles()
+    // ...then render results to page
+    .then(savedArticles => res.render('saved', { savedArticles }))
+    .catch(err => next(err))
 })
 
 module.exports = router
