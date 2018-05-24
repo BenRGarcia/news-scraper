699e8c428c5788352d869802893ea3604eff5ad6
Ben Garcia <ben@seebenprogram.com>
2018-05-24 14:15:43 -0400

    build home page route

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,10 @@ var getSavedArticles = require('./utils/query').getSavedArticles
  */
 
 // 'HOME' page
-router.get('/', (req, res, next) => {
-  // Retrieve articles from 'stagedArticles' collection (if any)...
-  // ...then render results to page
-  res.render('index', {})
-})
+router.get('/', (req, res, next) => res.render('index'))
 
 // 'SAVED ARTICLES' page
-router.get('/saved', (req, res, next) => {
+router.get('/articles/saved', (req, res, next) => {
   // Retrieve articles from 'savedArticles' collection (if any)...
   // ...then render results to page
   res.render('saved', {})
