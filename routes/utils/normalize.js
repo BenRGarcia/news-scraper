e31c81cd9013ef00c67125facfe1d7640aefcb33
Ben Garcia <ben@seebenprogram.com>
2018-05-24 14:16:00 -0400

    correct var name

--- routes/utils/normalize.js
+++ routes/utils/normalize.js
@@ -5,8 +5,8 @@ module.exports = html => {
   var $ = cheerio.load(html)
   $('div.latestnewsitem').each((i, el) => {
     var article = $(el).children().children('.col-lg-8')
-    var title = $(article).children('h4').children('a').attr('href').trim()
-    var link = $(article).children('h4').children('a').text()
+    var link = $(article).children('h4').children('a').attr('href').trim()
+    var title = $(article).children('h4').children('a').text()
     var preview = $(article).children('p').text().trim()
     results.push({ title, link, preview })
   })
