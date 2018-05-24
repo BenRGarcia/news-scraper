97eb06b087c6a6013506d25f191b3aad946ea7d3
Ben Garcia <ben@seebenprogram.com>
2018-05-24 15:49:31 -0400

    Build method to retrieve comments

--- public/javascripts/saved.js
+++ public/javascripts/saved.js
@@ -1,3 +1,16 @@
 $(function () {
+  const clearModal = () => $('#js-new-comment').empty()
 
+  const getArticleComments = async _id => $.get(`/api/scraper/article/${_id}/comments`)
+
+  const showModal = () => $('#js-modal-comments').modal('show')
+
+  $('body').on('click', 'button[data-comments]', e => {
+    clearModal()
+    const _id = $(e.target).data('id')
+    getArticleComments(_id)
+      .then(comments => console.log(comments))
+      .then(() => showModal())
+      .catch(err => console.error(err))
+  })
 })
